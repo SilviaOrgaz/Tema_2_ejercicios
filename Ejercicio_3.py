0b553f22133df8575a384f8ebbcf6c747876031c
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-09 22:28:33 +0200

    he añadido la suma a la lista

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -31,9 +31,10 @@ def modificar(lista, x):
             x.remove(i)
     print(f"Eliminar los números impares: {x}")
     print("Realizar la suma de los elementos que quedan: ", sum(x))
-    #x.extend(sum(x))
-
+    suma=sum(x)
+    x.append(suma)
+    print(f"Añadir como primer elemento de la lista la suma realizada: {x}")
     return x
 
 solucion = modificar(Lista, x)
-print(solucion)
+

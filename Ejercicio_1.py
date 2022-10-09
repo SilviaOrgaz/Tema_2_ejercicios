b1be512a8edc00107fc49490d412fb6efd093885
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-09 19:30:56 +0200

    probando eliminar los dobles

---
+++ Ejercicio_1.py
@@ -0,0 +1,15 @@
+
+#Ejercicio
+
+#Crea una clase llamada Punto con sus dos coordenadas X e Y.
+#Añade un método constructor para crear puntos fácilmente. Si no se reciben una coordenada, su valor será cero.
+#Sobreescribe el método string, para que al imprimir por pantalla un punto aparezca en formato (X,Y)
+#Añade un método llamado cuadrante que indique a qué cuadrante pertenece el punto, 
+# teniendo en cuenta que si X == 0 e Y != 0 se sitúa sobre el eje Y, si X != 0 e Y == 0 se sitúa sobre el eje X y si X == 0 e Y == 0 está sobre el origen.
+#Añade un método llamado vector, que tome otro punto y calcule el vector resultante entre los dos puntos.
+
+
+
+
+
+

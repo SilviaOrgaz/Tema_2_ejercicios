f2a57fbcf14433a5ed78613fa884f193c7f65c62
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-09 20:03:35 +0200

    He mejorado la lista

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -1,8 +1,11 @@
 ##Ejercicio 3 Crea una función modificar() que a partir de una lista de números realice las siguientes tareas sin modificar la original:
 
-# Borrar los elementos duplicados. Ordenar la lista de mayor a menor. 
-# Eliminar todos los números impares. Realizar una suma de todos los números que quedan. 
-# Añadir como primer elemento de la lista la suma realizada. Devolver la lista modificada.
+# Borrar los elementos duplicados.
+#  Ordenar la lista de mayor a menor. 
+# Eliminar todos los números impares. 
+# Realizar una suma de todos los números que quedan. 
+# Añadir como primer elemento de la lista la suma realizada.
+#  Devolver la lista modificada.
 # Finalmente, después de ejecutar la función, comprueba que la suma de todos los números a partir del segundo, 
 # concuerda con el primer número de la lista, 
 # tal que así: nueva_lista = modificar(lista)
@@ -22,13 +25,15 @@ def modificar(lista, x):
     for i in lista:
         if i not in x:
             x.append(i)
-            x.sort()
-    print(f"Borrar elementos duplicados y ordenar la lista de menor a mayor: {x}")
+    print(f"Borrar elementos duplicados: {x}")
+    x.sort()
+    print(f"Ordenar la lista de menor a mayor: {x}")
     for i in x:
         if i%2 != 0:
             x.remove(i)
-            sum(x)
-    print(f"Eliminar los números impares y realizar la suma de los elementos que quedan: {x}")
+    print(f"Eliminar los números impares: {x}")
+    sum(x)
+    print(f"Realizar la suma de los elementos que quedan: {x}")
     return x
 
 solucion = modificar(Lista, x)

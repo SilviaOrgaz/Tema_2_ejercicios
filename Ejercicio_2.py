dfe1a671850be3acfe641be03c1fc11fba94b583
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-12 17:16:38 +0200

    He añadido un punto al final de cada string

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -12,6 +12,9 @@
 #Ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro.
 #Lo único prohibido es modificar directamente el texto.
 
+from ctypes.wintypes import HLOCAL
+
+
 cadena = "un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
 
 #Esta función convierte una cadena en una lista de elementos que en la cadena principal están separados por una almuadilla
@@ -22,6 +25,13 @@ modificar= modificar_cadena(cadena)
 
 #for i in range(len(modificar)):
     #print(modificar[i])
+#Añade los puntos al final
+modificar[0]=modificar[0] + ".."
 
+for i in range(len(modificar)):
+    modificar[i]=modificar[i] + "."
+    
+#Imprime los elementos de la lista
 for i in modificar:
     print(i)
+

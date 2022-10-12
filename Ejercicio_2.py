4ffcab1956a39ea3249dc39e083a895d99b112b1
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-12 18:42:06 +0200

    he añadido las mayúsculas

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -12,9 +12,6 @@
 #Ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro.
 #Lo único prohibido es modificar directamente el texto.
 
-from ctypes.wintypes import HLOCAL
-
-
 cadena = "un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
 
 #Esta función convierte una cadena en una lista de elementos que en la cadena principal están separados por una almuadilla
@@ -26,8 +23,10 @@ modificar= modificar_cadena(cadena)
 modificar[0] += ".."
 for i in range(len(modificar)):
     modificar[i] += "."
+    modificar[i]= modificar[i][0].upper() + modificar[i][1:]
     
 #Imprime los elementos de la lista
 for i in modificar:
     print(i)
+
 

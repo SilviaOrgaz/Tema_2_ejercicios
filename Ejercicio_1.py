f85f4b4d0383daa3949a9719f383c99e08084ef1
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-16 22:29:21 +0200

    he añadido el enunciado

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -7,6 +7,19 @@
 #Añade un método llamado cuadrante que indique a qué cuadrante pertenece el punto, 
 # teniendo en cuenta que si X == 0 e Y != 0 se sitúa sobre el eje Y, si X != 0 e Y == 0 se sitúa sobre el eje X y si X == 0 e Y == 0 está sobre el origen.
 #Añade un método llamado vector, que tome otro punto y calcule el vector resultante entre los dos puntos.
+#Crea una clase llamada Rectangulo con dos puntos (inicial y final) que formarán la diagonal del rectángulo.
+#Añade un método constructor para crear ambos puntos fácilmente, si no se envían se crearán dos puntos en el origen por defecto.
+#Añade al rectángulo un método llamado base que muestre la base.
+#Añade al rectángulo un método llamado altura que muestre la altura.
+#Añade al rectángulo un método llamado area que muestre el area.
+#Experimentación
+#Crea los puntos A(2, 3), B(5,5), C(-3, -1) y D(0,0) e imprimelos por pantalla.
+#Consulta a que cuadrante pertenecen el punto A, C y D.
+#Consulta los vectores AB y BA.
+#(Optativo) Consulta la distancia entre los puntos 'A y B' y 'B y A'.
+#(Optativo) Determina cual de los 3 puntos A, B o C, se encuentra más lejos del origen, punto (0,0).
+#Crea un rectángulo utilizando los puntos A y B.
+#Consulta la base, altura y área del rectángulo.
 
 import math
 
